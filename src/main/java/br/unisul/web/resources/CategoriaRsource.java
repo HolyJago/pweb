658273bee2b6817b3421c9f877fcd5aa8b60aa2e
HolyJago <gustavo.gzr77@gmail.com>
2019-03-08 21:16:27 -0300

    Hast Categoria

---
+++ src/main/java/br/unisul/web/resources/CategoriaRsource.java
@@ -0,0 +1,26 @@
+package br.unisul.web.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.unisul.web.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaRsource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(new Categoria(1, "Informática"));
+		lista.add(new Categoria(2, "Camping"));
+		lista.add(new Categoria(3, "Automobilistica"));
+		lista.add(new Categoria(4, "Lazer"));
+		return lista;
+	}
+	
+}
